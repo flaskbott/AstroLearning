ddb18cbd2a8ab32affb1d6e6b91075e196f4f2be
flaskbott <122529832+flaskbott@users.noreply.github.com>
2023-10-01 14:37:49 +0800

    Update rss.xml.js

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function GET(context) {
   return rss({
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
-    site: context.site,
+    
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>en-us</language>`,
   });
